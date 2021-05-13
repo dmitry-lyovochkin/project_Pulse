ad96bc8cafff3ae564a9cdfe088779537377e7af
Dmitry <dmitry.lyovochkin@gmail.com>
2021-05-13 21:04:04 +0300

    не компилится код JS на табы, завтра гляну

--- src/js/script.js
+++ src/js/script.js
@@ -16,4 +16,10 @@ $(document).ready(function(){ /* обертка говорит что загру
             }      
         ]
     }); 
+// JQuery-tabs с сайта denis-creative.com с модификацией под себя
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('active');
+    });
 });
